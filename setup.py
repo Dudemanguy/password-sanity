4ba7cdc484b9f4ded6a65a16fc56c1d48551082e
Dudemanguy <random342@airmail.cc>
2020-07-02 19:21:05 -0500

    add bash/zsh completions
    Also alphabetize the arguments

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
+import sys
+from pathlib import Path
 from setuptools import setup
-setup(
-   name='password-sanity',
-   version='0.1',
-   description='Simple, secure, commandline password management',
-   author='Dudemanguy',
-   author_email='random342@airmail.cc',
-   url='https://github.com/Dudemanguy/password-sanity',
-   packages=['password-sanity'],
-   install_requires=['python-gnupg'],
-   scripts=[
-            'password-sanity/pws',
-           ]
-)
+
+def get_completions():
+   data_files = []
+   bash_dir = Path(sys.prefix) / "share" / "bash-completion" / "completions"
+   zsh_dir = Path(sys.prefix) / "share" / "zsh" / "site-functions"
+   if bash_dir.is_dir():
+      data_files.append((str(bash_dir), ["completions/bash/pws"]))
+   if zsh_dir.is_dir():
+      data_files.append((str(zsh_dir), ["completions/zsh/_pws"]))
+   return data_files
+
+def main():
+   completions = get_completions()
+
+   setup(
+      name='password-sanity',
+      version='0.1',
+      description='Simple, secure, commandline password management',
+      author='Dudemanguy',
+      author_email='random342@airmail.cc',
+      url='https://github.com/Dudemanguy/password-sanity',
+      packages=['password-sanity'],
+      install_requires=['python-gnupg'],
+      scripts=['password-sanity/pws',],
+      data_files = completions
+   )
+
+if __name__ == '__main__':
+    main()
