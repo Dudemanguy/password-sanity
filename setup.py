99fa2f8c2748473979f30eb07171a8f7db0d2c56
Dudemanguy <random342@airmail.cc>
2020-07-02 23:51:32 -0500

    setup.py: bump to version 0.3
    Very rapid release cycles but whatever. I think I got most of the
    features I want in here now.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 def main():
    setup(
       name='password-sanity',
-      version='0.2',
-      description='Simple, secure, commandline password management',
+      version='0.3',
+      description='A simple, python program for securely managing passwords through the command line.',
       author='Dudemanguy',
       author_email='random342@airmail.cc',
       url='https://github.com/Dudemanguy/password-sanity',
