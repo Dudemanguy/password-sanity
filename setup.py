9d2631250822dbc17c229b2d35d38692750fa24c
Dudemanguy <random342@airmail.cc>
2020-07-03 13:27:41 -0500

    setup.py: fix spacing
    For some reason, vim magically set shiftwidth to 3 instead of 4 (????)

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from pathlib import Path
 from setuptools import setup
 
 def main():
-   setup(
-      name='password-sanity',
-      version='0.3',
-      description='A simple, python program for securely managing passwords through the command line.',
-      author='Dudemanguy',
-      author_email='random342@airmail.cc',
-      url='https://github.com/Dudemanguy/password-sanity',
-      packages=['password-sanity'],
-      install_requires=['python-gnupg'],
-      scripts=['password-sanity/pws',],
-      package_data={'password-sanity': ['completions/bash/pws', 'completions/zsh/_pws']}
-   )
+    setup(
+        name='password-sanity',
+        version='0.3',
+        description='A simple, python program for securely managing passwords through the command line.',
+        author='Dudemanguy',
+        author_email='random342@airmail.cc',
+        url='https://github.com/Dudemanguy/password-sanity',
+        packages=['password-sanity'],
+        install_requires=['python-gnupg'],
+        scripts=['password-sanity/pws',],
+        package_data={'password-sanity': ['completions/bash/pws', 'completions/zsh/_pws']}
+    )
 
 if __name__ == '__main__':
     main()
