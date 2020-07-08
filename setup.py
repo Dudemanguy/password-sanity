cfd34fcbc136398167461f926c8ca568ffb8c78d
Dudemanguy <random342@airmail.cc>
2020-07-08 07:16:17 -0500

    setup.py: fix pyperclip pip package name

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 def main():
     setup(
         name='password-sanity',
-        version='0.4',
+        version='0.41',
         description='A simple, python program for securely managing passwords through the command line.',
         author='Dudemanguy',
         author_email='random342@airmail.cc',
         url='https://github.com/Dudemanguy/password-sanity',
         packages=['password-sanity'],
-        install_requires=['python-gnupg', 'python-pyperclip'],
+        install_requires=['python-gnupg', 'pyperclip'],
         scripts=['password-sanity/pws',],
         package_data={'password-sanity': ['completions/bash/pws', 'completions/zsh/_pws']}
     )
